d3b22c64b2b6287ed29fa1a2a1add0a4ab9e1f6d
Matteo Muscas <thatman.j@gmail.com>
2019-05-29 10:16:49 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -18,7 +18,7 @@ class ArtistsController < ApplicationController
   def create
     @artist = Artist.new(artist_params(:name, :bio))
 
-    @artist = Artist.new(artist _params(:name, :bio))
+    @artist = Artist.new(artist_params(:name, :bio))
     @artist.save
     redirect_to artist_path(@artist)
   end
